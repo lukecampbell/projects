b6ca69bba018b84408edff5e0ddd678d9d18cd16
Luke Campbell <luke.s.campbell@gmail.com>
2015-06-14 10:18:34 -0400

    Moves views folder to templates
    This is mostly to avoid confusion

--- projects/app.js
+++ projects/app.js
@@ -32,7 +32,7 @@ app.log = bunyan.createLogger({
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
