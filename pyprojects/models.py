0f65d988e195e210c4274810b4cc14d4c695b49a
Luke Campbell <luke.s.campbell@gmail.com>
2015-06-13 23:04:31 -0400

    Much better way of dealing with API Resources

--- pyprojects/models.py
+++ pyprojects/models.py
@@ -1,4 +1,6 @@
 from pyprojects import db
+from pyprojects.resource import Resource
+from flask import jsonify, request
 
 from collections import OrderedDict
 from datetime import datetime, date
@@ -20,5 +22,51 @@ class DictSerializableMixin(object):
             return v.isoformat()
         return v
 
-class Project(db.Model, DictSerializableMixin):
+class Project(db.Model, DictSerializableMixin, Resource):
     __tablename__ = 'projects'
+    required_fields = {
+        'id' : str,
+        'name' : unicode,
+        'description' : unicode,
+        'start_date' : date,
+        'manager': unicode,
+        'budget': float
+    }
+    optional_fields = {
+        'end_date' : date
+    }
+    record_key = 'projects'
+    path = '/api/project'
+    
+    @classmethod
+    def index(cls):
+        q = cls.query
+        if 'order_by' in request.args and request.args['order_by'] in cls.__table__.columns:
+            q = q.order_by(request.args['order_by'])
+        else:
+            q= q.order_by(cls.id)
+        records = [r.serialize() for r in q]
+        return jsonify({cls.record_key: records, "length": len(records)})
+
+
+class Budget(db.Model, DictSerializableMixin, Resource):
+    __tablename__ = 'budgets'
+    required_fields = {
+        'project_id' : str,
+        'total_budget' : float,
+        'spent_budget' : float
+    }
+    optional_fields = {
+        'created_at' : datetime
+    }
+    record_key = 'budgets'
+    path = '/api/budget'
+
+    @classmethod
+    def index(cls):
+        q = cls.query
+        if 'project_id' in request.args:
+            q.filter_by(project_id=request.args['project_id'])
+        records = [r.serialize() for r in q.all()]
+        return jsonify({cls.record_key: records, "length": len(records)})
+
